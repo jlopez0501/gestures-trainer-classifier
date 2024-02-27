73f3ee8e8b298932c96891b6df3b6cfb42595fd9
Oleksii Mosolab <mosolab@gmail.com>
2024-02-27 23:41:03 +0100

    Added initial project structure

---
+++ python/plot_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+
+# Replace 'data.csv' with the actual name of your CSV file
+file_path = 'data.csv'
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Plot the data
+    plt.figure(figsize=(10, 6))
+
+    # Plot each column
+    plt.plot(df['q0'], label='q0')
+    plt.plot(df['q1'], label='q1')
+    plt.plot(df['q2'], label='q2')
+    plt.plot(df['q3'], label='q3')
+
+    # Add labels and title
+    plt.xlabel('Index')
+    plt.ylabel('Values')
+    plt.title('Plotting Data from CSV File')
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
---
+++ python/serial_read_icm20948_host.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: 2021 Carter Nelson for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import argparse
+import re
+import serial
+import time
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("port", type=str, help="Serial port of the board", nargs=1)
+args = parser.parse_args()
+port = args.port
+
+# open serial port (NOTE: change location as needed)
+ss = serial.Serial(args.port[0])
+
+# read string
+_ = ss.readline() # first read may be incomplete, just toss it
+
+f = open("data.csv", 'w')
+f.write("deltaTime,q0,q1,q2,q3\n")
+
+while True:
+    raw_string = ss.readline().strip().decode()
+
+    j = json.loads(raw_string)
+    print(j)
+    s = str(j['deltaTime'] + ',' + j['quat_w']) + ',' + str(j['quat_x']) + ',' + str(j['quat_y']) + ',' + str(j['quat_z']) + '\n'
+    f.write(s)
+
+    '''
+    # create list of floats
+    data = [float(x) for x in raw_string.split(',')]
+
+    # print them
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" %(data[0], data[1], data[2])) 
+    '''
