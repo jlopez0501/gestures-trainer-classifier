dfcd4b8a445c718c7f2a8e04887871452e582f84
Jlopez <143033778+Jlopez2024@users.noreply.github.com>
2024-03-30 20:59:34 -0400

    Improve serial script

--- python/serial_read_icm20948_host.py
+++ python/serial_read_icm20948_host.py
@@ -16,50 +16,76 @@ port = args.port
 # open serial port (NOTE: change location as needed)
 ss = serial.Serial(args.port[0])
 
-# read string
-_ = ss.readline() # first read may be incomplete, just toss it
-
-#### Exercise ####
-# file_name = "front_raise" + input("Front raise set number: ") + ".csv"
-# file_name = "shoulder_press" + input("Shoulder press set number: ") + ".csv"
-# file_name = "curl" + input("Curl set number: ") + ".csv"
-# file_name = "side_raise" + input("Side raise set number: ") + ".csv"
-# file_name = "french_press" + input("French press set number: ") + ".csv"
-# file_name = "dumbell_row" + input("Dumbell row set: ") + ".csv"
-# file_name = "barbell_bench_press" + input("Barbell bench press set: ") + ".csv"
-file_name = "rope_tricep_pushdown" + input("Rope tricep pushdown set: ") + ".csv"
-
-##################
-
-#### Exercise Directory ####
-# f = open("../DMP_9D_ACCEL_Logs/front_raise_3_10/" + file_name, 'w')
-# f = open("../DMP_9D_ACCEL_Logs/shoulder_press/" + file_name, 'w')
-# f = open("../DMP_9D_ACCEL_Logs/curl_3_10/" + file_name, 'w')
-# f = open("../DMP_9D_ACCEL_Logs/side_raise_3_8/" + file_name, 'w')
-# f = open("../DMP_9D_ACCEL_Logs/french_press_3_8/" + file_name, 'w')
-# f = open("../DMP_9D_ACCEL_Logs/dumbell_row_3_10/" + file_name, 'w')
-# f = open("../DMP_9D_ACCEL_Logs/barbell_bench_press_3_13/" + file_name, 'w')
-f = open("../DMP_9D_ACCEL_Logs/rope_tricep_pushdown_3_13/" + file_name, 'w')
-############################
-
-f.write("timeMs,ax,ay,az,q0,q1,q2,q3\n")
+loop_counter = 0
+set_number = 0
 
 while True:
-    # New try statement
     try:
-        raw_string = ss.readline().strip().decode()
+        # read string
+        _ = ss.readline() # first read may be incomplete, just toss it
+
+        # Set number logic
+        if loop_counter == 0:
+            set_number = int(input("Specify initial set number: "))
+            print(set_number)
+        else:
+            new_set_number = input("New set number (Press enter to increment): ")
+            if new_set_number == "":
+                set_number += 1
+                print(set_number)
+            else:
+                set_number = new_set_number
+
         
-        j = json.loads(raw_string)
-        print(j)
-        s = str(j['timeMs']) + ',' + str(j['accel_x']) + ',' + str(j['accel_y']) + ',' + str(j['accel_z']) + ',' + str(j['quat_w']) + ',' + str(j['quat_x']) + ',' + str(j['quat_y']) + ',' + str(j['quat_z']) + '\n'
-        f.write(s)
+        ##### Exercise ###    
+        file_name = f"front_raise_3_30_front_raise{set_number}.csv"
+        # file_name = "shoulder_press_3_29_shoulder_press" + input("Shoulder press set number: ") + ".csv"
+        # file_name = "curl" + input("Curl set number: ") + ".csv"
+        # file_name = "side_raise" + input("Side raise set number: ") + ".csv"
+        # file_name = "french_press" + input("French press set number: ") + ".csv"
+        # file_name = "dumbell_row" + input("Dumbell row set: ") + ".csv"
+        # file_name = "barbell_bench_press" + input("Barbell bench press set: ") + ".csv"
+        # file_name = "rope_tricep_pushdown" + input("Rope tricep pushdown set: ") + ".csv"
 
-        '''
-        # create list of floats
-        data = [float(x) for x in raw_string.split(',')]
+        ##################
 
-        # print them
-        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" %(data[0], data[1], data[2])) 
-        '''
+
+
+        #### Exercise Directory ####
+
+        # f = open("../DMP_9D_ACCEL_Logs/front_raise/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/shoulder_press/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/curl_3_10/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/side_raise_3_8/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/french_press_3_8/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/dumbell_row_3_10/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/barbell_bench_press_3_13/" + file_name, 'w')
+        # f = open("../DMP_9D_ACCEL_Logs/rope_tricep_pushdown_3_13/" + file_name, 'w')
+        f = open("../DMP_9D_ACCEL_Logs/TEST/" + file_name, 'w')
+        ############################
+
+        f.write("timeMs,ax,ay,az,q0,q1,q2,q3\n")
+        
+        while True:
+            # New try statement
+            try:
+                raw_string = ss.readline().strip().decode()
+                
+                j = json.loads(raw_string)
+                print(j)
+                s = str(j['timeMs']) + ',' + str(j['accel_x']) + ',' + str(j['accel_y']) + ',' + str(j['accel_z']) + ',' + str(j['quat_w']) + ',' + str(j['quat_x']) + ',' + str(j['quat_y']) + ',' + str(j['quat_z']) + '\n'
+                f.write(s)
+
+                '''
+                # create list of floats
+                data = [float(x) for x in raw_string.split(',')]
+
+                # print them
+                print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" %(data[0], data[1], data[2])) 
+                '''
+            except KeyboardInterrupt:
+                loop_counter += 1
+                break
     except KeyboardInterrupt:
+        print("Exiting...")
         break
